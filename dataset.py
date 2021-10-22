c70fb832f85f4dac9b64907ab2b47c9b6109e1ee
Younghoon-Lee <8109h@naver.com>
2021-10-22 10:05:28 +0000

    hotfix

--- dataset.py
+++ dataset.py
@@ -50,7 +50,7 @@ class CustomDataLoader(Dataset):
             # Background = 0
             masks = np.zeros((image_infos["height"], image_infos["width"]))
             # General trash = 1, ... , Cigarette = 10
-            anns = sorted(anns, key=lambda idx : len(idx['area']), reverse=True)
+            anns = sorted(anns, key=lambda idx : idx['area'], reverse=True)
             for i in range(len(anns)):
                 className = get_classname(anns[i]['category_id'], cats)
                 pixel_value = category_names.index(className)
