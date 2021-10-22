374a109002e244a3be544f6c50a57a3d6d24f00d
jinwoo <tjdwnsdud3@naver.com>
2021-10-22 09:24:28 +0000

    modify mmsegmentaion and ann

--- mmsegmentation/configs/jinwoo/default_runtime.py
+++ mmsegmentation/configs/jinwoo/default_runtime.py
@@ -4,13 +4,15 @@ log_config = dict(
     hooks=[
         dict(type='TextLoggerHook', by_epoch=False),
         dict(type='WandbLoggerHook',
-            init_kwargs=dict(project='mmsegmentation', entity='carry-van', name='hrnet'))
+            init_kwargs=dict(project='mmsegmentation', entity='carry-van', name='hrnetw48_40k'))
         # dict(type='TensorboardLoggerHook')
     ])
 # yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
 cudnn_benchmark = True
+work_dir = '/opt/ml/segmentation/baseline/mmsegmentation/work_dirs/jinwoo4'
